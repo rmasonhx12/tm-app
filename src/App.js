0c5ac287698cf35b773a148486bf70cd0c39b216
Robert Hendricks <rmasonh22@gmail.com>
2024-01-26 12:35:14 -0500

    rack

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+  <h1>Confirmation Message</h1>
+  <p>Thank you for your reservation. An email confirmation will be sent to you shortly.</p>
+  <p>Meta Front End Developer Final Example for Ux-Ui Course</p>
+</div>
+
+)}
+export default App;
